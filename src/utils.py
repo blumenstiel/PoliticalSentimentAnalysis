1381ac143ef2abe0db4e6894bc19bb62af232894
Benedikt Blumenstiel <benedikt.blumenstiel@kit.edu>
2021-05-08 15:05:59 +0200

    add sample dataset

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
 from nltk.probability import FreqDist
+import random
+import pandas as pd
+import numpy as np
 
 def get_common_words(texts, count=10):
     # save all text in one list
@@ -22,3 +25,22 @@ def get_common_hashtags(texts, count):
     fdist = FreqDist(w.lower() for w in all_texts if w.startswith('#') and len(w) > 1)
 
     return fdist.most_common(count)
+
+
+def sample_dataset(df, column, num_samples):
+    """
+    Get sampled dataset
+    """
+    # init
+    samples = pd.DataFrame()
+    num_labels = len(df[column].unique())
+
+    random.seed(42)
+
+    # get samples for each label in column of df
+    for label in df[column].unique():
+        label_df = df[df[column] == label]
+        label_df = label_df.loc[random.sample(set(label_df.index), min(int(num_samples / num_labels), len(label_df)))]
+        samples = pd.concat([samples, label_df])
+
+    return samples
